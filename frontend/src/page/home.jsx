a96b32681e11b89f2a5600085ec07dea97cdbf3a
shbhakta <shbhakta@shbhaktas-MacBook-Pro.local>
2025-04-11 11:15:13 +0530

    modified UI - shresth

--- frontend/src/page/home.jsx
+++ frontend/src/page/home.jsx
@@ -4,35 +4,37 @@ import './style.scss';
 
 const HomePage = () => {
   return (
+    
     <div className="home-page">
       <header>
-        <img src="frontend/src/assets/dialologo.png" alt="Welcome Logo" className="logo" />
+        <img src="../src/assets/dialo.png" alt="Welcome Logo" className="logo" />
         <h1>Welcome to Dialo</h1>
       </header>
       <section className="bots-section">
         <h2>Different Bots:</h2>
         <div className="bot-boxes">
           <Link to="/bot1" className="box">
-            <h3>Bot 1</h3>
-            <p>Go to Bot 1</p>
+            {/* <h3>Bot 1</h3> */}
+            <p>Order Management BOT</p>
           </Link>
           <Link to="/bot2" className="box">
-            <h3>Bot 2</h3>
-            <p>Go to Bot 2</p>
+            {/* <h3>Bot 2</h3> */}
+            <p>Marketing BOT</p>
           </Link>
-          <Link to="/bot3" className="box">
+          {/* <Link to="/bot3" className="box">
             <h3>Bot 3</h3>
             <p>Go to Bot 3</p>
-          </Link>
+          </Link> */}
         </div>
       </section>
       <footer className="footer">
         <div className="team-info">
           <h3>Team Members:</h3>
           <ul>
-            <li>Abhay Kumar</li>
-            <li>Rakshith</li>
-            <li>Shresth Bhakta</li>
+            <div>Abhay Kumar</div>
+            <div>Rakshith</div>
+            <div>Shresth Bhakta</div>
+            <div>Gaurav Kumawat</div>
           </ul>
         </div>
         <div className="team-name">
@@ -42,7 +44,7 @@ const HomePage = () => {
         <div className="contact-us">
           <h3>Contact Us:</h3>
           <p>Email: [info@dialo.com](mailto:info@dialo.com)</p>
-          <p>Phone: +1-123-456-7890</p>
+          <p>Phone: +91 7990377120</p>
         </div>
         <div className="copyright">
           <p>&copy; 2024 Dialo Team. All rights reserved.</p>
